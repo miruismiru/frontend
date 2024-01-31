29b73e56db06c1aa059cbdead2f07cfa1b883c6e
omgdaddy <itzmiru@gmail.com>
2024-01-31 10:01:26 +0500

    as

--- pages/pwa/more.vue
+++ pages/pwa/more.vue
@@ -13,42 +13,21 @@
       <h1>More</h1>
     </div>
     <v-list lines="two">
-      <v-list-item title="DOTstream's PWA mode" subtitle="Version amvN3 (Nuxt 3)">
+      <v-list-item title="" subtitle="">
         <template #prepend>
           <img
-            src="/icon.png"
+            src="/logo.png"
             style="display: flex; width: 100px; height: 100%"
           />
         </template>
       </v-list-item>
-      <v-list-item
-        title="DOTstream"
-        :subtitle="new Date().getFullYear() + ' © DOTstream'"
-      />
+     
     </v-list>
     <v-divider class="my-4"></v-divider>
     <v-list lines="two">
-      <v-list-item title="Web Mode" append-icon="mdi-web" to="/"></v-list-item>
       <v-list-item title="About us" target="blank" href="/about" />
-      <v-list-item title="Privacy Policy" target="blank" href="/privacy" />
-      <v-list-item
-        title="Help"
-        target="blank"
-        href="https://docs.amvstr.ml/help"
-      >
-        <template #append>
-          <v-icon icon="mdi-help" />
-        </template>
-      </v-list-item>
-      <v-list-item
-        title="Github"
-        target="blank"
-        href="https://github.com/DOTstream/DOTstream"
-      >
-        <template #append>
-          <v-icon icon="mdi-github" />
-        </template>
-      </v-list-item>
+     
+      
     </v-list>
   </v-container>
 </template>
