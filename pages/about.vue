ac4d40732461a778cccb52d3f39b3a4d07668186
omgdaddy <itzmiru@gmail.com>
2024-01-31 14:44:53 +0500

    sasa

--- pages/about.vue
+++ pages/about.vue
@@ -19,8 +19,9 @@ useHead({
 </script>
 <template>
   <v-container>
-    <div style="height: fit-content; display: grid; place-items: center">
-      <div class="pa-5" style="background-color: #212121">
+    <div style="height: fit-content; display: grid; place-items: center; background-color: #212121; border-radius: 50px; padding: 20px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+      <div class="pa-5" style="background-color: #212121; border-radius: 0px;">
+      
         <h1><img src="/logo.png" alt="loho" height="50" /></h1>
         <p>
           i will think about something to write or you can send me something to write ... 
@@ -28,10 +29,12 @@ useHead({
         <div class="mt-2">
          
         </div>
-        <div class="my-5">
+        <div class="my-">
           <h1>Donation</h1>
           We do not use ADs and rely on donation to keep the website running forever. 
           You can consider donating us to keep it alive! <br>
+
+          discord / @is_coffee
          
         </div>
       </div>
