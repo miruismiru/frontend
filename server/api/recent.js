054abaf141ae9037ac726c33f3a4e73c69dfb22d
omgdaddy <itzmiru@gmail.com>
2024-02-04 15:35:44 +0500

    dsadssad

--- server/api/recent.js
+++ server/api/recent.js
@@ -14,31 +14,18 @@ export default defineEventHandler(async (event) => {
       fetchedData = await response.json();
     }
 
-  
     for (var i = 0; i < fetchedData.results.length; i++) {
-
-  let tt = fetchedData.results[i++].id.replaceAll('-'," ")
-
+      let tt = fetchedData.results[i++].id.replaceAll('-', '');
 
       const response = await fetch(`https://api-amvstrm.nyt92.eu.org/api/v2/search/?q=${tt}&limit=1`);
-  
+
       if (!response.ok) {
-          throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok');
       }
-  
+
       search = await response.json();
-      // done.push(search.results)
-
-  }
-  
-     
-    
-
-
-
-
-
-
+      done.push(search.results);
+    }
 
     return {
       status: 200,
@@ -51,8 +38,8 @@ export default defineEventHandler(async (event) => {
       statusCode: 500,
       statusMessage: 'server error',
     });
+  } finally {
+    // Clear the done array regardless of success or failure
     done = [];
-
   }
-
 });
